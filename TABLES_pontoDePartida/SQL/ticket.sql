ff380d240009ba76b38510da2a179747612610cd
artistasuave <michelle.jrog@gmail.com>
2018-08-16 10:11:57 -0300

    ticket

---
+++ TABLES_pontoDePartida/SQL/ticket.sql
@@ -0,0 +1,19 @@
+
+
+CREATE TABLE ticket(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	id_empresa INT NOT NULL,
+	FOREIGN KEY (id_empresa) references empresa (id),
+	id_criticidade INT NOT NULL,
+	FOREIGN KEY (id_colaborador) references colaborador (id),
+	id_ticket_sazonalidade INT NOT NULL,
+	FOREIGN KEY (id_ticket_sazonalidade) references ticket_sazonalidade (id),
+	situacao VARCHAR (100),
+	titulo VARCHAR (150),
+	data_abertura DATE NOT NULL,
+	sistemaOperacional VARCHAR (50),
+	versaoBanco VARCHAR (10),
+	descricao TEXT,
+	data_encerramento DATE NOT NULL,
+	procedimento_resolucao TEXT
+);
