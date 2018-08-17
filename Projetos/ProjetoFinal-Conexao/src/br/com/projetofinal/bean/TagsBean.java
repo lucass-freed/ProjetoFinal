553c411b5d456de0407568caa1a49425ebc5ebf9
matheus ruan werner <matheusbnu50@hotmail.com>
2018-08-17 08:32:07 -0300

    tags

--- Projetos/ProjetoFinal-Conexao/src/br/com/projetofinal/bean/TagsBean.java
+++ Projetos/ProjetoFinal-Conexao/src/br/com/projetofinal/bean/TagsBean.java
@@ -8,5 +8,21 @@ public class TagsBean {
     
     private int id;
     private String titulo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
 }
