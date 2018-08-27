bfe3027da567d7252b3980b908af73000c928eff
matheus ruan werner <matheusbnu50@hotmail.com>
2018-08-27 10:05:52 -0300

    Criacao pacote web Regular

--- Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/master/EmpresaIndex.java
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/master/EmpresaIndex.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.projetofinal.web;
+package br.com.projetofinal.web.master;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
