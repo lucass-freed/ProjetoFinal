90b2c7bf0877ee512ad2c5b01d881b58e7039859
Alunos <gabriel-dms@hotmail.com>
2018-08-31 09:56:13 -0300

    criação de empresa dao externo

---
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/dao/EmpresaDAO.java
@@ -0,0 +1,51 @@
+
+package br.com.projetofinal.dao;
+
+import br.com.projetofinal.bean.EmpresaBean;
+import br.com.projetofinal.database.Conexao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Thiago
+ */
+public class EmpresaDAO {
+    
+    public EmpresaBean obterDadosEmpresa(int id){
+        String sql = "SELECT * FROM empresas WHERE id = ?";
+        try{
+            PreparedStatement ps = Conexao.getConnection().prepareStatement(sql);
+            ps.setInt(1, id);
+            ps.execute();
+            ResultSet resultSet = ps.getResultSet();
+            if(resultSet.next()){
+                EmpresaBean empresa = new EmpresaBean();
+                empresa.setId(id);
+                empresa.setCnpj(resultSet.getString("cnpj"));
+                empresa.setRazaoSocial(resultSet.getString("razao_social"));
+                empresa.setNomeFantasia(resultSet.getString("nome_fantasia"));
+                empresa.setInscricaoEstadual(resultSet.getString("inscricao_estadual"));
+                empresa.setEmail(resultSet.getString("email"));
+                empresa.setTelefone(resultSet.getString("telefone"));
+                empresa.setLogradouro(resultSet.getString("logradouro"));
+                empresa.setNumero(resultSet.getString("numero"));
+                empresa.setComplemento(resultSet.getString("complemento"));
+                empresa.setBairro(resultSet.getString("bairro"));
+                empresa.setCep(resultSet.getString("cep"));
+                empresa.setCidade(resultSet.getString("cidade"));
+                empresa.setUf(resultSet.getString("uf"));
+                empresa.setDataAtivacao(resultSet.getString("data_ativacao"));
+                empresa.setDataExpiracao(resultSet.getString("data_expiracao"));
+                empresa.setValidadeCertificado(resultSet.getString("validade_certificado"));
+                return empresa;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally{
+            Conexao.closeConnection();
+        }
+        return null;
+    }
+}
