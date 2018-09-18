cc471750d9853cdeff866f1416c2b1630268adb4
avancini21 <thiagoentra21@gmail.com>
2018-09-18 10:28:44 -0300

    construção de servlet

---
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/usuario/UsuarioPerfil.java
@@ -0,0 +1,35 @@
+package br.com.projetofinal.web.usuario;
+
+import br.com.projetofinal.bean.EmpresaBean;
+import br.com.projetofinal.bean.UsuarioBean;
+import br.com.projetofinal.dao.EmpresaDAO;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Thiago Avancini
+ */
+@WebServlet("/usuario/perfil")
+public class UsuarioPerfil extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getSession().getAttribute("usuario") == null){
+            resp.sendRedirect("/");
+        }
+        
+        UsuarioBean usuario = (UsuarioBean) req.getSession().getAttribute("usuario");
+
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setAttribute("usuario", usuario);  
+        req.setAttribute("title", "Perfil");
+        req.getRequestDispatcher("/perfil/perfil-index.jsp").include(req, resp);
+
+    }
+    
+}
