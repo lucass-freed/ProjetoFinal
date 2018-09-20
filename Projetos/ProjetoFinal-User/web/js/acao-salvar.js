ccbc918ece702a8a09b6b52a5c7abde905eb19f7
Thiago Avancini <thiagoavancini@outlook.com>
2018-09-20 19:01:46 -0300

    inclusão de parâmetos ajax

--- Projetos/ProjetoFinal-User/web/js/acao-salvar.js
+++ Projetos/ProjetoFinal-User/web/js/acao-salvar.js
@@ -1,11 +1,22 @@
 $("#botao-salvar").on("click", function () {
         $.ajax({
-            url: "/empresa-externo/buscaempresa?id=1",
+            url: "/empresa-externo/buscaempresa",
             method: "post",
             data: {
                 "cnpj": $("#inputBasicCNPJ").val(),
-                "razao_social": $("#inputBasicRazaoSocial").val(),
-                "id": $("#inputBasicId").val()
+                "razaoSocial": $("#inputBasicRazaoSocial").val(),
+                "id": $("#inputBasicId").val(),
+                "nomeFantasia": $("inputNomeFantasia").val(),
+                "inscricaoEstadual": $("inputIE").val(),
+                "email": $("inputBasicEmail").val(),
+                "telefone": $("inputPhone").val(),
+                "logradouro": $("inputBasicLogradouro").val(),
+                "numero": $("inputBasicNumero").val(),
+                "complemento": $("inputBasicComplemento").val(),
+                "bairro": $("inputBasicBairro").val(),
+                "cep": $("inputBasicCEP").val(),
+                "uf": $("selectBasicUf").val(),
+                "uf": $("selectBasicUf").val(),
             },
             success: function (result) {
 
