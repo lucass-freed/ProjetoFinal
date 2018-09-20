ce8addcd7956deef8eafd1cc59403cfc14954f15
lucass-freed <lucassfreed@hotmail.com>
2018-09-20 11:23:00 -0300

    11:22

--- Projetos/ProjetoFinal-User/web/js/consulta.js
+++ Projetos/ProjetoFinal-User/web/js/consulta.js
@@ -1,8 +1,25 @@
 $(function () {
     $("#campo-ticket").select2({
+        placeholder: "Pesquisar por título",
         ajax: {
             url: "/ticket/obtertodosparaselect2",
-            dataType: "json"
+            dataType: "json",
+            data: function (params) {
+                return {
+                    q: params.term // search term
+                };
+            },
         }
     });
+    $("#campo-ticket").change(function () {
+        
+        var e = document.getElementById("select2-campo-ticket-results");
+        var strUser = e.options[e.selectedIndex].value;
+        alert(strUser);
+        alert("dsas");
+
+        //window.location.replace("teste");
+
+    });
+
 });
