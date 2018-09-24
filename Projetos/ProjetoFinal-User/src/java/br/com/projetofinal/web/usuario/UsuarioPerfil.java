0c327c84df7069c21ea5bbedf85212e54ba06673
lucass-freed <lucassfreed@hotmail.com>
2018-09-24 07:54:56 -0300

    07:35

--- Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/usuario/UsuarioPerfil.java
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/usuario/UsuarioPerfil.java
@@ -1,6 +1,5 @@
 package br.com.projetofinal.web.usuario;
 
-import br.com.projetofinal.bean.UsuarioBean;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,24 +9,20 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Thiago Avancini
+ * @author Lucas Rodrigo Frederico (lucassfreed@hotmail.com)
  */
-@WebServlet("/externo/usuario/perfil")
-public class UsuarioPerfil extends HttpServlet{
-
+@WebServlet("/externo/perfil")
+public class UsuarioPerfil extends HttpServlet {
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(req.getSession().getAttribute("usuario") == null){
+        if (req.getSession().getAttribute("usuario") == null) {
             resp.sendRedirect("/");
             return;
         }
-        
-        UsuarioBean usuario = (UsuarioBean) req.getSession().getAttribute("usuario");
-        resp.setContentType("text/html;charset=UTF-8");
-        req.setAttribute("usuario", usuario);  
-        req.setAttribute("title", "Perfil");
-        req.getRequestDispatcher("/perfil/perfil-index.jsp").include(req, resp);
 
+        resp.setContentType("text/html;charset=UTF-8");
+        req.setAttribute("title", "Perfil");
+        req.getRequestDispatcher("/perfil/index.jsp").include(req, resp);
     }
-    
 }
