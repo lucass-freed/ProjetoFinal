18e8c7c0b059f4fdf09ee6d2112fb6a4b990ce78
Thiago Avancini <thiagoavancini@outlook.com>
2018-09-24 19:40:58 -0300

    alterações

--- Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/ticket/chamado/ChamadoStore.java
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/web/ticket/chamado/ChamadoStore.java
@@ -2,8 +2,13 @@ package br.com.projetofinal.web.ticket.chamado;
 
 import br.com.projetofinal.bean.TicketBean;
 import br.com.projetofinal.bean.TicketTagBean;
+import br.com.projetofinal.dao.TicketDAO;
+import br.com.projetofinal.dao.TicketTagDAO;
+import br.com.projetofinal.enumTypes.CriticidadeTypes;
+import br.com.projetofinal.enumTypes.EnumTicketStatusType;
 import br.com.projetofinal.web.ticket.Ticket;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.Arrays;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,6 +31,26 @@ public class ChamadoStore extends HttpServlet {
         }
         
         TicketBean ticket = new TicketBean();
+        ticket.setId(Integer.parseInt(req.getParameter("id")));
+        ticket.setIdEmpresa(Integer.parseInt(req.getParameter("idEmpresa")));
+        ticket.setIdColaborador(Integer.parseInt(req.getParameter("id_colaborador")));
+        ticket.setIdSazonalidade(Integer.parseInt(req.getParameter("id_ticket_sazonalidade")));
+        ticket.setIdUsuario(Integer.parseInt(req.getParameter("id_usuario")));
+        ticket.setTitulo(req.getParameter("titulo"));
+        ticket.setCriticidade(CriticidadeTypes.valueOf(req.getParameter("criticidade")));
+        ticket.setStatus(EnumTicketStatusType.valueOf(req.getParameter("situacao")));
+        ticket.setDescricao(req.getParameter("descricao"));
+        ticket.setSistemaOperacional(req.getParameter("sistemaOperacional"));
+        ticket.setVersaoBanco(req.getParameter("versaoBanco"));
+        ticket.setDataAbertura(Date.valueOf(req.getParameter("data_ativacao")));
+        
+        int codigo = new TicketDAO().inserir(ticket);
+        
+        if(codigo > 0){
+            resp.sendRedirect("/externo/chamado");
+        }
+        
+        
         // popular ticket com as info do form
         // cadastrar ticket 
         
