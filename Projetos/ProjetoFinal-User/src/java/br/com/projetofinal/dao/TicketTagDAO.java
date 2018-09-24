460233ced31e53b27e01de6384946b0bcb721324
Thiago Avancini <thiagoavancini@outlook.com>
2018-09-24 18:20:23 -0300

    criação de DAO

---
+++ Projetos/ProjetoFinal-User/src/java/br/com/projetofinal/dao/TicketTagDAO.java
@@ -0,0 +1,36 @@
+package br.com.projetofinal.dao;
+
+import br.com.projetofinal.bean.TicketTagBean;
+import br.com.projetofinal.database.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Thiago
+ */
+public class TicketTagDAO {
+    
+    public int inserir(TicketTagBean ticketTag){
+        Connection conexao = Conexao.getConnection();
+        if(conexao != null){
+            String sql = "INSERT INTO ticket_tags (id_tickets, id_tags) VALUES (?, ?);";
+            try{
+                PreparedStatement ps = conexao.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+                int quantidade = 1;
+                ps.setInt(quantidade++, ticketTag.getIdTickets());
+                ps.setInt(quantidade++, ticketTag.getIdTags());
+                ps.execute();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                Conexao.closeConnection();
+            }
+        }
+        return 0;
+    }
+}
