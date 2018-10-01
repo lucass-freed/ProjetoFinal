de6811263b446d284804318c88051f5dca80bf3d
Gabriel Ferreira <gabrieloferreira@hotmail.com>
2018-10-01 01:01:11 -0300

    Arrumando os JUnit

--- Projetos/ProjetoFinal-Interno/test/br/com/projetofinal/JUnit/TicketDAOJUnitTest.java
+++ Projetos/ProjetoFinal-Interno/test/br/com/projetofinal/JUnit/TicketDAOJUnitTest.java
@@ -7,6 +7,7 @@ import br.com.projetofinal.enumTypes.CriticidadeTypes;
 import br.com.projetofinal.enumTypes.EnumTicketStatusType;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Date;
+import java.sql.Timestamp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
@@ -25,15 +26,13 @@ public class TicketDAOJUnitTest {
         TicketBean t = new TicketBean();
         t.setIdEmpresa(1);
         t.setIdColaborador(1);
-        t.setIdSazonalidade(1);
+        t.setSazonalidade("dssadsa");
         t.setTitulo("dssadsa");
         t.setCriticidade(CriticidadeTypes.ALTISSIMA);
         t.setStatus(EnumTicketStatusType.CONCLUIDO);
         t.setDescricao("dsadas");
-        t.setSistemaOperacional("dsaads");
-        t.setVersaoBanco("dsadsa");
-        t.setDataAbertura(Date.valueOf("1993-12-30"));
-        t.setDataEncerramento(Date.valueOf("1993-12-30"));
+        t.setDataAbertura(Timestamp.valueOf("1993-12-30"));
+        t.setDataEncerramento(Timestamp.valueOf("1993-12-30"));
         t.setProcedimentoResolucao("dsadsa");
 
         int cod = new TicketDAO().inserir(t);
@@ -50,15 +49,13 @@ public class TicketDAOJUnitTest {
         TicketBean t = new TicketBean();
         t.setIdEmpresa(1);
         t.setIdColaborador(1);
-        t.setIdSazonalidade(1);
+        t.setSazonalidade("dssadsa");
         t.setTitulo("dssadsa");
         t.setCriticidade(CriticidadeTypes.ALTISSIMA);
         t.setStatus(EnumTicketStatusType.CONCLUIDO);
         t.setDescricao("dsadas");
-        t.setSistemaOperacional("dsaads");
-        t.setVersaoBanco("dsadsa");
-        t.setDataAbertura(Date.valueOf("1993-12-30"));
-        t.setDataEncerramento(Date.valueOf("1993-12-30"));
+        t.setDataAbertura(Timestamp.valueOf("1993-12-30"));
+        t.setDataEncerramento(Timestamp.valueOf("1993-12-30"));
         t.setProcedimentoResolucao("dsadsa");
 
         int cod = new TicketDAO().inserir(t);
@@ -76,15 +73,13 @@ public class TicketDAOJUnitTest {
         t.setId(cod);
         t.setIdEmpresa(1);
         t.setIdColaborador(1);
-        t.setIdSazonalidade(1);
+        t.setSazonalidade("dssadsa");
         t.setTitulo("dssadsa");
         t.setCriticidade(CriticidadeTypes.ALTISSIMA);
         t.setStatus(EnumTicketStatusType.CONCLUIDO);
         t.setDescricao("dsadas");
-        t.setSistemaOperacional("dsaads");
-        t.setVersaoBanco("dsadsa");
-        t.setDataAbertura(Date.valueOf("1993-12-30"));
-        t.setDataEncerramento(Date.valueOf("1993-12-30"));
+        t.setDataAbertura(Timestamp.valueOf("1993-12-30"));
+        t.setDataEncerramento(Timestamp.valueOf("1993-12-30"));
         t.setProcedimentoResolucao("dsadsa");
 
         new TicketDAO().alterar(t);
@@ -98,15 +93,13 @@ public class TicketDAOJUnitTest {
         TicketBean t = new TicketBean();
         t.setIdEmpresa(1);
         t.setIdColaborador(1);
-        t.setIdSazonalidade(1);
+        t.setSazonalidade("dssadsa");
         t.setTitulo("dssadsa");
         t.setCriticidade(CriticidadeTypes.ALTISSIMA);
         t.setStatus(EnumTicketStatusType.CONCLUIDO);
         t.setDescricao("dsadas");
-        t.setSistemaOperacional("dsaads");
-        t.setVersaoBanco("dsadsa");
-        t.setDataAbertura(Date.valueOf("1993-12-30"));
-        t.setDataEncerramento(Date.valueOf("1993-12-30"));
+        t.setDataAbertura(Timestamp.valueOf("1993-12-30"));
+        t.setDataEncerramento(Timestamp.valueOf("1993-12-30"));
         t.setProcedimentoResolucao("dsadsa");
 
         int cod = new TicketDAO().inserir(t);
@@ -139,13 +132,11 @@ public class TicketDAOJUnitTest {
     public void validarIgual(TicketBean t1, TicketBean t2) {
         assertEquals(t1.getId(), t2.getId());
         assertEquals(t1.getIdColaborador(), t2.getIdColaborador());
-        assertEquals(t1.getIdSazonalidade(), t2.getIdSazonalidade());
+        assertEquals(t1.getSazonalidade(), t2.getSazonalidade());
         assertEquals(t1.getTitulo(), t2.getTitulo());
         assertEquals(t1.getCriticidade(), t2.getCriticidade());
         assertEquals(t1.getStatus(), t2.getStatus());
         assertEquals(t1.getDescricao(), t2.getDescricao());
-        assertEquals(t1.getSistemaOperacional(), t2.getSistemaOperacional());
-        assertEquals(t1.getVersaoBanco(), t2.getVersaoBanco());
         assertEquals(t1.getDataAbertura(), t2.getDataAbertura());
         assertEquals(t1.getDataEncerramento(), t2.getDataEncerramento());
         assertEquals(t1.getProcedimentoResolucao(), t2.getProcedimentoResolucao());
